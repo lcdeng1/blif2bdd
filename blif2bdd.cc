0432b6c76b297aa80aa3145d23a97bae2176a2c7
lcdeng1 <lichuan.deng97@hotmail.com>
2023-11-15 23:26:33 -0600

    more information and add number of building outputs flag

--- blif2bdd.cc
+++ blif2bdd.cc
@@ -162,6 +162,15 @@ unsigned determine_levels(symbol* IN, unsigned ordering)
     return num_vars;
 }
 
+unsigned determine_outputs(symbol* IN)
+{
+    unsigned num_vars = 0;
+    for (const symbol* p = IN; p; p=p->next) {
+        if (p->type == OUTPUT) ++num_vars;
+    }
+    return num_vars;
+}
+
 uint64_t mark_and_count(rexdd_forest_t *F, rexdd_edge_t *e)
 {
     unmark_forest(F);
@@ -315,6 +324,8 @@ int usage(const char* exe)
     std::cerr << "    -t: The BDD type for building (default: RexBDD)\n";
     typelist();
     std::cerr << "\n";
+    std::cerr << "    -o: The number of outputs to build\n";
+    std::cerr << "\n";
     std::cerr << "    -g: Enable garbage collection\n";
     std::cerr << "\n";
     std::cerr << "    -f: The histoy results will be writen into file [BDD_name].txt\n";
@@ -339,9 +350,11 @@ int usage(const char* exe)
  */
 int main(int argc, char** argv) {
     char bdd_type = 0;      // default RexBDD: 0
+    int outputs = 0;
     bool is_gc = false;
     bool is_history = false;
     bool display = false;
+    bool funcheck = false;
     bool show_card = false;
     bool testlex = false;
     bool testparse = false;
@@ -363,6 +376,10 @@ int main(int argc, char** argv) {
             display = true;
             continue;
         }
+        if (0==strcmp("-v", argv[i])) {
+            funcheck = true;
+            continue;
+        }
         if (0==strcmp("-c", argv[i])) {
             show_card = true;
             continue;
@@ -370,6 +387,11 @@ int main(int argc, char** argv) {
         if (0==strcmp("-t", argv[i])) {
             i++;
             bdd_type = std::stoi(argv[i]);
+            continue;
+        }
+        if (0==strcmp("-p", argv[i])) {
+            i++;
+            outputs = std::stoi(argv[i]);
             continue;
         }
         if (0==strcmp("-L", argv[i])) {
@@ -429,7 +451,7 @@ int main(int argc, char** argv) {
         return 0;
     }
     unsigned num_vars = determine_levels(inlist, ordering);
-
+    unsigned num_outs = determine_outputs(slist);
     //
     // now ready to initialize BDD forest
     //
@@ -450,6 +472,27 @@ int main(int argc, char** argv) {
         }
     }
 
+    // Following used for trace the building process, flag setting TBD
+    // clean the existed operation calls files
+    std::string calls_fileName = F.S.type_name;
+    calls_fileName += "operationCalls_Sum.txt";
+    FILE* calls_file = fopen(calls_fileName.c_str(), "r");
+    if (calls_file) {
+        // it exists, empty it
+        fclose(calls_file);
+        calls_file = fopen(calls_fileName.c_str(), "w+");
+        fclose(calls_file);
+    }
+    calls_fileName = F.S.type_name;
+    calls_fileName += "operationCalls_Prod.txt";
+    calls_file = fopen(calls_fileName.c_str(), "r");
+    if (calls_file) {
+        // it exists, empty it
+        fclose(calls_file);
+        calls_file = fopen(calls_fileName.c_str(), "w+");
+        fclose(calls_file);
+    }
+
     //
     //  Build BDD for .inputs
     //
@@ -463,12 +506,19 @@ int main(int argc, char** argv) {
     //
     //  Build BDD from outputs
     //
+    rexdd_edge_t out_dd[num_outs+1];
+    unsigned out_idx = 0;
+    uint64_t peak_num = 0;
+    uint64_t pre_ANDs = 0, pre_AND_CTs = 0, pre_NOTs = 0, pre_NOT_CTs = 0, pre_peak = 0;
     timer* rtime = new timer;
     for (symbol* p=slist; p; p=p->next) {
         //
         if (p->type == OUTPUT) {
             // std::cerr << "building " << p->name << "...\n";
+            timer* intime = new timer;
             p->build_bdd(&F);
+            out_dd[out_idx] = p->dd;
+            out_idx++;
             if (display) {
                 std::cerr << "\t" << p->name << ":\n";
                 show_edge(p->dd);
@@ -479,10 +529,51 @@ int main(int argc, char** argv) {
                 }
                 uint64_t num_nodes;
                 num_nodes = mark_and_count(&F, &p->dd);
-                std::cerr << "number of nodes: " << num_nodes << "\n";
+                std::cerr << "number of nodes: \t" << num_nodes << "\n";
+                peak_num = F.UT->num_entries;
+                std::cerr << "peak nodes: \t" << peak_num << "\n";
+                std::cerr << "inc nodes: \t";
+                if (peak_num >= pre_peak) {
+                    std::cerr << peak_num - pre_peak << "\n";
+                } else {
+                    std::cerr << "-" << pre_peak - peak_num << "\n";
+                }
+                std::cerr << "AND calls: \t" << F.num_ops-pre_ANDs << "\n";
+                std::cerr << "AND CT hits: \t" << F.ct_hits-pre_AND_CTs << "\n";
+                std::cerr << "NOT calls: \t" << F.num_nots-pre_NOTs << "\n";
+                std::cerr << "NOT CT hits: \t" << F.ct_hits_nots - pre_NOT_CTs << "\n";
+                intime->note_time();
+                std::cerr << "Time interval: \t" << intime->get_last_seconds() << " seconds\n";
+                pre_ANDs = F.num_ops;
+                pre_AND_CTs = F.ct_hits;
+                pre_NOTs = F.num_nots;
+                pre_NOT_CTs = F.ct_hits_nots;
+                pre_peak = peak_num;
             }
-            
-            // if (p->name == "615(1750)") {
+
+            if (funcheck) {
+                // check the function results
+                FILE* fout;
+                std::string filename = L.getModelName();
+                filename += "_";
+                filename += p->name;
+                filename += "_";
+                filename += F.S.type_name;
+                filename += "functionOut.txt";
+                fout = fopen(filename.c_str(), "w+");
+                bool minterm_eval[num_vars+2];
+                minterm_eval[0] = 0;
+                minterm_eval[num_vars+1] = 0;
+                 for (uint64_t i=0; i<=0x01LL<<num_vars; i++) {
+                    for (unsigned int j=0; j<num_vars; j++) {
+                        minterm_eval[j+1] = i & (0x01LL << j);
+                    }
+                    fprintf(fout, "%d\n", rexdd_eval(&F, &p->dd, num_vars, minterm_eval));
+                }
+                fclose(fout);
+            }
+
+            // if (p->name == "V138(3)") {
             //     FILE* fout;
             //     std::string filename = "out_edge";
             //     filename += F.S.type_name;
@@ -505,11 +596,12 @@ int main(int argc, char** argv) {
                     }
                 }
                 //
+                if (F.UT->num_entries > peak_num) peak_num = F.UT->num_entries;
                 rexdd_sweep_UT(F.UT);
                 rexdd_sweep_CT(F.CT, F.M);
                 rexdd_sweep_nodeman(F.M);
-                // std::cerr << "Done GC!\n";
             }
+            if (outputs != 0 && out_idx == outputs) break;
         }
     }
     rtime->note_time();
@@ -518,9 +610,13 @@ int main(int argc, char** argv) {
     //  counting the number of nodes
     //
     unmark_forest(&F);
-    for (symbol* p=slist; p; p=p->next) {
-        if (p->type == OUTPUT) {
-            mark_nodes(&F, p->dd.target);
+    if (outputs == 0) {
+        for (unsigned int i=0; i<num_outs; i++) {
+            mark_nodes(&F, out_dd[i].target);
+        }
+    } else {
+        for (unsigned int i=0; i<outputs; i++) {
+            mark_nodes(&F, out_dd[i].target);
         }
     }
     uint_fast64_t q, n;
@@ -535,9 +631,20 @@ int main(int argc, char** argv) {
     } // for p
     
     if (!is_history) {
+        std::cerr << "========================Final(" << F.S.type_name << ")==========================\n";
         std::cerr << "Model: " << L.getModelName() << "\n";
-        std::cerr << "Total number of nodes in " << F.S.type_name <<": " << num_nodes << "\n";
-        std::cerr << "Time taken of " << F.S.type_name << ": \t\t" << rtime->get_last_seconds() << " seconds\n";
+        std::cerr << "Number of inputs: \t" << num_vars << "\n";
+        std::cerr << "Number of outputs: \t" << num_outs << "\n";
+        std::cerr << "Peak nodes: \t\t" << peak_num << "\n";
+        std::cerr << "Final nodes number: \t" << num_nodes << " <==\n";
+        std::cerr << "Total running time: \t" << rtime->get_last_seconds() << " seconds <==\n";
+        std::cerr << "Total AND calls: \t" << F.num_ops << "\n";
+        std::cerr << "Total AND terminals: \t" << F.num_terms << "\n";
+        std::cerr << "Total AND CT hits: \t" << F.ct_hits << "\n";
+        std::cerr << "Total NOT calls: \t" << F.num_nots << "\n";
+        std::cerr << "Total NOT CT hits: \t" << F.ct_hits_nots << "\n";
+        std::cerr << "Mallocs in CT: \t\t" << F.CT->num_entries << "\n";
+        std::cerr << "Overwrites in CT: \t" << F.CT->num_overwrite << "\n";
     } else {
         FILE* fout;
         std::string filename = F.S.type_name;
@@ -546,6 +653,14 @@ int main(int argc, char** argv) {
         fprintf(fout, "Model\t%s\n", L.getModelName().c_str());
         fprintf(fout, "Nodes#\t%llu\n", num_nodes);
         fprintf(fout, "Time\t%f\n", rtime->get_last_seconds());
+        fprintf(fout, "Peak\t%llu\n", peak_num);
+        fprintf(fout, "ANDs\t%llu\n", F.num_ops);
+        fprintf(fout, "AND_terms\t%llu\n", F.num_terms);
+        fprintf(fout, "AND_CTs\t%llu\n", F.ct_hits);
+        fprintf(fout, "NOTs\t%llu\n", F.num_nots);
+        fprintf(fout, "NOT_CTs\t%llu\n", F.ct_hits_nots);
+        fprintf(fout, "CT_mallocs\t%llu\n", F.CT->num_entries);
+        fprintf(fout, "CT_overWs\t%llu\n", F.CT->num_overwrite);
         fclose(fout);
         std::cerr << "Done!\n";
     }
